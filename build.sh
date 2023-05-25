848263a5ddfa44e5d8c9e855b1c2c00c6a96ac08
Vrutik <vrutikprajapati2809@gmail.com>
2023-05-26 04:31:36 +0530

    test undo

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 
 set -o errexit  # exit on error
 
-sudo dpkg -l mongodb-database-tools
-sudo apt install ./mongodb-database-tools-*-100.7.1.deb
-
 pip install --upgrade pip
 pip install -r requirements.txt
 
